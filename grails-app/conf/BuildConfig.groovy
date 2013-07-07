c8df89af895a908e87dd0180a8dd130d096f6cc2
Vlad <v-eliseev@yandex.ru>
2013-07-08 00:57:29 +0400

    joda-time moved to dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,6 +27,8 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.21'
+        compile 'joda-time:joda-time:2.2'
+
     }
 
     plugins {
