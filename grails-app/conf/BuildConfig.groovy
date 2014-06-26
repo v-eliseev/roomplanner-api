16fcd01a4ee0da30038d2931fdedd57a51969fe3
Vlad <v-eliseev@yandex.ru>
2014-06-26 23:36:41 +0400

    updates

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -53,7 +53,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":release:3.0.1",
-              ":rest-client-builder:2.0.1") {
+              ":rest-client-builder:2.0.3") {
             export = false
         }
     }
